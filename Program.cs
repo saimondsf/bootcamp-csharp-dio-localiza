9a2541464ad7e441dc4daa63773779fbb0003656
Saimon <saimon.dsfg@gmail.com>
2021-04-05 21:05:34 -0300

    Standard program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BootcampLocaliza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
