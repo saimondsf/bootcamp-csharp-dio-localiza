4aea1f2fb1207e5373a864a5e9b87713b674dd47
Saimon <saimon.dsfg@gmail.com>
2021-04-05 22:25:31 -0300

    First modification - loop 'for'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace BootcampLocaliza
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 9;
+
+            for(int i=0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
         }
     }
 }
