8ebaf64ae56a521885a204b946274e6bad3db8a7
Saimon <saimon.dsfg@gmail.com>
2021-04-19 12:02:12 -0300

    Adding basic command file

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace BootcampLocaliza
         {
             int numeroDeVezes = 9;
 
+            int[] a = new int [3];
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+            int[] t = a;
+
             for(int i=0; i < numeroDeVezes; i++){
                 Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
             }
